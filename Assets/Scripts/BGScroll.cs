2fc94ff4d558c168245e0158cd363b340feae6f8
Ben <benhoughta@hotmail.com>
2017-07-17 14:16:14 +1000

    Added running animation, moving background and foreground.

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2((Time.time * speed) % 1, 0f);
+    }
+}
